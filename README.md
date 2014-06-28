0dd970bdca7a40dbf401de82bc215d5f413d2f6d
Lloyd Henning <lloyd.w.henning@gmail.com>
2014-06-28 13:27:31 +0100

    Flash white when all black

--- README.md
+++ README.md
@@ -2,3 +2,7 @@ infinite-trailer
 ================
 
 trailer forever
+
+Requires opencv python bindings. Download them using your system package manager
+or from the OpenCV site.
+
--- infinite-trailer.py
+++ infinite-trailer.py
@@ -10,6 +10,10 @@ while(True):
     # Our operations on the frame come here
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
+    number_of_zeros = np.count_nonzero(gray)
+    if number_of_zeros == 0:
+        gray[0:100] = 255
+
     # Display the resulting frame
     cv2.imshow('frame',gray)
     if cv2.waitKey(1) & 0xFF == ord('q'):
