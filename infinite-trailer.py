9d8cfa77a8e009277b661f43d200e641dc19bf26
mariosgohan <marios_katsak@outlook.com>
2014-06-28 14:59:27 +0100

    Write file

--- infinite-trailer.py
+++ infinite-trailer.py
@@ -6,6 +6,13 @@ seen_black = False
 start_black = []
 end_black = []
 frame_number = 0
+current_clip_number = 0
+
+def get_video_writer(clip_number):
+    fourcc = cv2.cv.CV_FOURCC(*'XVID')
+    out = cv2.VideoWriter('clip%d.avi' % clip_number,fourcc, 20.0, (640,480))
+    return out
+writer = get_video_writer(current_clip_number)
 while(True):
     # Capture frame-by-frame
     ret, frame = cap.read()
@@ -18,10 +25,17 @@ while(True):
         gray[0:100,0:100] = 255
         seen_black = True
         start_black.append(frame_number)
+        writer.release()
     elif number_of_zeros > 0 and seen_black:
         end_black.append(frame_number)
         seen_black = False
-
+        current_clip_number += 1
+        writer = get_video_writer(current_clip_number)
+    
+    if number_of_zeros > 0:
+        print 'wrote frame'
+        writer.write(frame)
+        
     # Display the resulting frame
     cv2.imshow('frame',gray)
     if cv2.waitKey(1) & 0xFF == ord('q'):
@@ -32,4 +46,5 @@ print start_black
 print end_black	
 # When everything done, release the capture
 cap.release()
+writer.release()
 cv2.destroyAllWindows()
