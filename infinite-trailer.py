c78ac24bd4ff03515034672d62acf36042f4e31e
Lloyd Henning <lloyd.w.henning@gmail.com>
2014-06-28 15:28:47 +0100

    Don't loop forever

--- infinite-trailer.py
+++ infinite-trailer.py
@@ -25,6 +25,9 @@ writer = get_video_writer(current_clip_number)
 while(True):
     # Capture frame-by-frame
     ret, frame = cap.read()
+    if not ret:
+        # End of the video
+        break
 
     # Our operations on the frame come here
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
